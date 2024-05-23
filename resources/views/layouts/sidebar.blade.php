4724429a0a68f33d7b357b143f4b99c91e7080e5
Dayutirta <dayumahara502@gmail.com>
2024-05-23 22:40:44 +0700

    New theme

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,70 +1,70 @@
-<div class="sidebar">
-    <!-- SidebarSearch Form -->
-    {{-- <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                aria-label="Search">
-            <div class="input-group-append">
-                <button class="btn btn-sidebar">
-                    <i class="fas fa-search fa-fw"></i>
-                </button>
-            </div>
-        </div>
-    </div> --}}
-
+<div class="sidebar" >
     <!-- Sidebar Menu -->
     <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-            data-accordion="false">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
                 <a href="{{ url('/') }}" class="nav-link">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
-                    <p>
-                        Dashboard
-                    </p>
+                    <i class="nav-icon fas fa-home"></i>
+                    <p>Dashboard</p>
                 </a>
+            </li>
+
             <li class="nav-header">Menu Administrasi</li>
-            <li class="nav-item">
-                <a href="{{ url('/level') }}" class="nav-link ">
-                    <i class="nav-icon fas fa-layer-group"></i>
-                    <p>
-                        Data Warga
-                    </p>
-                </a>
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            Data Warga
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ url('/level') }}" class="nav-link">
+                            <i class="fas fa-user-shield nav-icon"></i>
+                            <p>Level</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ url('/user') }}" class="nav-link">
+                            <i class="fas fa-user-edit nav-icon"></i>
+                            <p>User</p>
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="nav-item">
-                <a href="{{ url('/user') }}" class="nav-link ">
-                    <i class="nav-icon far fa-user"></i>
-                    <p>
-                        Cetak Data Warga
-                    </p>
-                </a>
-            </li>
+
             <li class="nav-header">Menu Bantuan Sosial</li>
-            <li class="nav-item">
-                <a href="{{ url('/kategori') }}" class="nav-link ">
-                    <i class="nav-icon far fa-bookmark"></i>
+            <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-hand-holding-heart"></i>
                     <p>
-                        Data Bansos
+                        Bantuan Sosial
+                        <i class="right fas fa-angle-left"></i>
                     </p>
                 </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ url('/kategori') }}" class="nav-link">
+                            <i class="fas fa-database nav-icon"></i>
+                            <p>Data Bansos</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ url('/barang') }}" class="nav-link">
+                            <i class="fas fa-users nav-icon"></i>
+                            <p>Data Penerima</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ url('/stok') }}" class="nav-link">
+                            <i class="fas fa-print nav-icon"></i>
+                            <p>Cetak Data Penerima</p>
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="nav-item">
-                <a href="{{ url('/barang') }}" class="nav-link">
-                    <i class="nav-icon far fa-list-alt"></i>
-                    <p>
-                        Data Penerima
-                    </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ url('/stok') }}" class="nav-link ">
-                    <i class="nav-icon fas fa-cubes"></i>
-                    <p>
-                        Cetak Data Penerima
-                    </p>
-                </a>
-            </li>
+
         </ul>
     </nav>
     <!-- /.sidebar-menu -->
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WargaController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,16 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [WelcomeController::class, 'index']);
+// Route::get('/', [AuthController::class, 'index'])->name('login');
+// Route::post('login', [AuthController::class, 'proses_login'])->name('proses_login');
+// Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+// Route::group(['middleware' => ['auth']], function () {
+//     route::group(['middleware' => ['cek_login:1,2']], function () {
+//         Route::resource('admin', WargaController::class);
+//     });
 
-Route::get('/', [AuthController::class, 'index'])->name('login');
-Route::post('login', [AuthController::class, 'proses_login'])->name('proses_login');
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-Route::group(['middleware' => ['auth']], function () {
-    route::group(['middleware' => ['cek_login:1,2']], function () {
-        Route::resource('admin', WargaController::class);
-    });
-
-    route::group(['middleware' => ['cek_login:3']], function () {
-        Route::resource('user', WargaController::class);
-    });
-});
+//     route::group(['middleware' => ['cek_login:3']], function () {
+//         Route::resource('user', WargaController::class);
+//     });
+// });
