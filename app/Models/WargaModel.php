4b38fd73eb1a0e3dc60cae699bc50097914de70f
Haidarly2 <haidar.aly007@gmail.com>
2024-05-23 08:51:54 +0700

    Mengisi warga model

--- app/Models/WargaModel.php
+++ app/Models/WargaModel.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class WargaModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'm_warga';
+
+    protected $primaryKey = 'nik';
+
+    protected $fillable = [
+        'nik',
+        'id_level',
+        'nokk',
+        'nama',
+        'jenis_kelamin',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'alamat',
+        'agama',
+        'status',
+        'kewarganegaraan',
+        'pekerjaan',
+        'pendidikan',
+        'status_pernikahan',
+    ];
+
+    public function level()
+    {
+        return $this->belongsTo(LevelModel::class, 'id_level', 'id_level');
+    }
 }
