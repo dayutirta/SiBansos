5be808d4bb051f6e9c1c2ec3d8efa381a7b81652
Haidarly2 <haidar.aly007@gmail.com>
2024-05-23 11:55:06 +0700

    Memberikan middleware login

--- app/Http/Middleware/Cek_login.php
+++ app/Http/Middleware/Cek_login.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class Cek_login
@@ -11,10 +12,23 @@ class Cek_login
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $roles
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $roles): Response
     {
-        return $next($request);
+        if (!Auth::check()) {
+            return redirect('login');
+        }
+
+        $user = Auth::user();
+
+        if ($user->id_level == $roles) {
+            return $next($request);
+        }
+
+        return redirect('login')->with('error', 'Anda tidak memiliki akses');
     }
 }
