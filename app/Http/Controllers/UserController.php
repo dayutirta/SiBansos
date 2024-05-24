f55f9e5476322418f38211f9ec55606349bb04f5
Haidarly2 <haidar.aly007@gmail.com>
2024-05-24 11:06:11 +0700

    Memindahkan controller untuk user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WargaModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // dd(session()->all());
+        $user = Auth::user();
+
+        if ($user->id_level == '1') {
+            $breadcrumb = (object) [
+                'title' => 'Admin Page',
+                'list' => [
+                    'Home',
+                ]
+            ];
+    
+            $page = (object) [
+                'title' => 'Selamat Datang di Halaman Dashboard',
+            ];
+    
+            $activeMenu = 'dashboard';
+    
+            $level = WargaModel::all();
+    
+            return view('admin.index', [
+                'breadcrumb' => $breadcrumb,
+                'page' => $page,
+                'level' => $level,
+                'activeMenu' => $activeMenu
+            ]);
+        } elseif ($user->id_level == '2') {
+            $breadcrumb = (object) [
+                'title' => 'Admin Page',
+                'list' => [
+                    'Home',
+                ]
+            ];
+    
+            $page = (object) [
+                'title' => 'Selamat Datang di Halaman Dashboard',
+            ];
+    
+            $activeMenu = 'dashboard';
+    
+            $level = WargaModel::all();
+    
+            return view('rt.index', [
+                'breadcrumb' => $breadcrumb,
+                'page' => $page,
+                'level' => $level,
+                'activeMenu' => $activeMenu
+            ]);
+        } else if ($user->id_level == '3') {
+            $breadcrumb = (object) [
+                'title' => 'Page Warga',
+                'list' => [
+                    'Home',
+                ]
+            ];
+    
+            $page = (object) [
+                'title' => 'Selamat datang di halaman warga',
+            ];
+    
+            $activeMenu = 'user';
+    
+            $level = WargaModel::all();
+    
+            return view('user.index', [
+                'breadcrumb' => $breadcrumb,
+                'page' => $page,
+                'level' => $level,
+                'activeMenu' => $activeMenu
+            ]);
+        }
+    }
+}
