b0032284cced54adc747d90c1e3d5f42bff5149e
Haidarly2 <haidar.aly007@gmail.com>
2024-05-24 20:00:40 +0700

    Menambahkan Function create dan store

--- app/Http/Controllers/WargaController.php
+++ app/Http/Controllers/WargaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
 use App\Models\WargaModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -31,9 +32,9 @@ class WargaController extends Controller
     {
         $warga = WargaModel::with('level');
 
-        // if ($request->nik) {
-        //     $warga->where('nik', $request->nik);
-        // }
+        if ($request->id_level) {
+            $warga->where('id_level', $request->id_level);
+        }
 
         return DataTables::of($warga)
             ->addIndexColumn()
@@ -46,5 +47,69 @@ class WargaController extends Controller
             })
             ->rawColumns(['aksi'])
             ->make(true);
+    }
+
+    public function create(){
+        $breadcrumb = (object) [
+            'title' => 'Tambah Warga',
+            'list' => [
+                'Home',
+                'Warga',
+                'Tambah Warga'
+            ]
+        ];
+    
+        $page = (object) [
+            'title' => 'Tambah Warga',
+        ];
+    
+        $level = LevelModel::all();
+    
+        $activeMenu = 'warga';
+    
+        return view('admin.warga.create', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'level' => $level,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+    
+    public function store(Request $request){
+        $request->validate([
+            'id_level' =>'required|integer',
+            'nik' =>'required|string',
+            'nokk' =>'required|string',
+            'nama' =>'required|string',
+            'jenis_kelamin' =>'required|string',
+            'tempat_lahir' =>'required|string',
+            'tanggal_lahir' =>'required|date',
+            'alamat' =>'required|string',
+            'agama' =>'required|string',
+            'status' =>'required|string',
+            'kewarganegaraan' =>'required|string',
+            'pekerjaan' =>'required|string',
+            'pendidikan' =>'required|string',
+            'status_pernikahan' =>'required|string'
+        ]);
+    
+        WargaModel::create([
+            'id_level' => $request->id_level,
+            'nik' => $request->nik,
+            'nokk' => $request->nokk,
+            'nama' => $request->nama,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'alamat' => $request->alamat,
+            'agama' => $request->agama,
+            'status' => $request->status,
+            'kewarganegaraan' => $request->kewarganegaraan,
+            'pekerjaan' => $request->pekerjaan,
+            'pendidikan' => $request->pendidikan,
+            'status_pernikahan' => $request->status_pernikahan
+        ]);
+    
+        return redirect('/warga')->with('success', 'Data berhasil ditambahkan');
     }
 }
