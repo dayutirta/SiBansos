6426bdb671665596dedc3d6a18c39e951c55c1f4
Haidarly2 <haidar.aly007@gmail.com>
2024-06-02 01:40:15 +0700

    Membuat tampilan dashboard untuk warga

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,24 +1,83 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col-12 col-sm-4 col-md-3 text-center text-md-left">
-                            <img src="{{ asset('adminlte/dist/img/1.png') }}" alt="SiBansos Logo" class="img-fluid rounded-circle" style="max-width: 100%; height: auto; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);">
-                        </div>                       
-                        <div class="col-12 col-sm-8 col-md-9">
-                            <h1 class="display-4 mb-4">Selamat Datang, {{ Auth::user()->nama }}</h1>
-                            <p class="lead">Silahkan klik menu yang tersedia.</p>
-                            <a href="{{ route('logout') }}" class="btn btn-danger">Logout</a>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-12 col-sm-4 col-md-3 text-center text-md-left">
+                                <img src="{{ asset('adminlte/dist/img/1.png') }}" alt="SiBansos Logo"
+                                    class="img-fluid rounded-circle"
+                                    style="max-width: 100%; height: auto; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);">
+                            </div>
+                            <div class="col-12 col-sm-8 col-md-9">
+                                <h1 class="display-4 mb-4">Selamat Datang, {{ Auth::user()->nama }}</h1>
+                                <p class="lead">Silahkan klik menu yang tersedia.</p>
+                                <a href="{{ route('logout') }}" class="btn btn-danger">Logout</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <h5 class="mb-2">
+            Info Administrasi
+        </h5>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Messages</span>
+                        <span class="info-box-number">1,410</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Bookmarks</span>
+                        <span class="info-box-number">410</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Uploads</span>
+                        <span class="info-box-number">13,648</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Likes</span>
+                        <span class="info-box-number">93,139</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+        </div>
     </div>
-</div>
 @endsection
