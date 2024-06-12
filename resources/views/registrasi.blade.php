6369a56ccd358ceedeffdba9c5ab720e2ecbcf40
Dayutirta <dayumahara502@gmail.com>
2024-06-13 01:04:03 +0700

    pembuatan registrasi user

---
+++ resources/views/registrasi.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registrasi</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+    <!-- Logo Icon -->
+    <link rel="icon" type="image/png" href="{{ asset('adminlte/dist/img/SiB_new.png') }}">
+</head>
+
+<body class="hold-transition register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <span class="brand-text font-weight-bold">SiBansos</span>
+        </div>
+
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="login-box-msg">Silahkan Mendaftarkan diri ke Pak RT/Pak RW dengan membawa KK Asli, KTP, Surat Pindah, dan Bukti Kepemilikan Rumah atau kontrak/sewa Rumah</p>
+                <p class="text-center">Atau bisa menghubungi no berikut : +628123456789</p>
+
+                
+                    
+                    <p class="mt-3 mb-1 text-center">
+                        Sudah punya akun? <a href="{{ route('login') }}">Login disini</a>
+                    </p>
+                    
+                    </div>
+                    <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                    </div>
+                    <!-- /.register-box -->
+                    
+                    <!-- jQuery -->
+                    <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+                    <!-- Bootstrap 4 -->
+                    <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+                    <!-- AdminLTE App -->
+                    <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+                    </body>
+                    
+                    </html>
+                    
