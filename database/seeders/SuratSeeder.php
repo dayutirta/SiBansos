5359fe735f47755c24833453660b453f300cb7fc
Dayutirta <dayumahara502@gmail.com>
2024-06-22 18:36:47 +0700

    membuat seeder baru

---
+++ database/seeders/SuratSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SuratSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('m_surat')->insert([
+            [
+                'kode_surat' => 'SKCK',
+                'nama_surat' => 'Surat Pengantar Pembuatan SKCK',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_surat' => 'SKD',
+                'nama_surat' => 'Surat Pengantar Pembuatan Keterangan Domisili',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_surat' => 'KTP/KK',
+                'nama_surat' => 'Surat Pengantar Pembuatan KTP/KK',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_surat' => 'SKTM',
+                'nama_surat' => 'Surat Pengantar Keterangan Tidak Mampu (SKTM)',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
